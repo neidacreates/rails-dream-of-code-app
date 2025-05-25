b0d03c24d85cf28a84aca6c4a1fc4bc3f665f926
Neida <neida@duck.com>
2025-05-24 23:23:54 -0400

    edit admin dashboard controller and index

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,6 +1,6 @@
 class AdminDashboardController < ApplicationController
     def index
         @current_trimester = Trimester.where("start_date <= ?", Date.today).where("end_date >= ?", Date.today).first
-        @future_trimester = Trimester.where("start_date < ?", Date.today + 6.months).where("end_date >= ?", Date.today + 6.months).first
+        @future_trimester = Trimester.where("start_date < ?", Date.today + 6.months).where("start_date >= ?", Date.today).first
     end  
 end
